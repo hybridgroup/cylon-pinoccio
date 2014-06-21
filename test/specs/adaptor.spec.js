4e7cd45664b131771150158a8cb892d3d5e8f744
deadprogram <ron@hybridgroup.com>
2014-06-20 18:30:23 -0700

    Initial commit

---
+++ test/specs/adaptor.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var Pinoccio = source("adaptor");
+
+describe("Cylon.Adaptors.Pinoccio", function() {
+  var adaptor = new Pinoccio();
+
+  it("needs tests");
+});
---
+++ test/specs/driver.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Pinoccio = source("driver");
+
+describe("Cylon.Drivers.Pinoccio", function() {
+  var driver = new Pinoccio({
+    device: { connection: 'connect' }
+  });
+
+  it("needs tests");
+});
