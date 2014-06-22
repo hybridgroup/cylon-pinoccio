fe4c14537312c5aa01a6ba5270ea737a4af0c3d9
deadprogram <ron@hybridgroup.com>
2014-06-21 17:15:27 -0700

    Example that changes LED color based on remaining power percent

---
+++ examples/power_monitor.js
@@ -0,0 +1,27 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: { 
+  	name: 'pinoccio', 
+  	adaptor: 'pinoccio', 
+  	token: 'XXX', 
+  	troop: '1', 
+  	scout: '1' },
+  devices: [{name: 'led', driver: 'pinoccio-led'},
+           {name: 'power', driver: 'pinoccio-power'}],
+
+  work: function(my) {
+    my.led.blue();
+    every((2).seconds(), function() {
+      my.power.percent(function(err, data){
+        if(data < 10) {
+          my.led.red();
+        } else if(data < 20) {
+          my.led.yellow();
+        } else {
+          my.led.green();
+        }
+      });
+    });
+  }
+}).start();
