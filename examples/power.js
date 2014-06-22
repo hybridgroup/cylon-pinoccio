55a441b74d6789e9d011a35f59c40327a56b2a41
deadprogram <ron@hybridgroup.com>
2014-06-21 17:01:28 -0700

    We really can retrieve power info now

---
+++ examples/power.js
@@ -0,0 +1,20 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: { 
+  	name: 'pinoccio', 
+  	adaptor: 'pinoccio', 
+  	token: 'XXX', 
+  	troop: '1', 
+  	scout: '1' },
+  device: {name: 'power', driver: 'pinoccio-power'},
+
+  work: function(my) {
+    every((3).seconds(), function() {
+      my.power.voltage(function(err, data){
+        console.log(err);
+        console.log(data);
+      });
+    });
+  }
+}).start();
---
+++ test/specs/pinoccio-power.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var PinoccioPower = source("pinoccio-power");
+
+describe("Cylon.Drivers.PinoccioPower", function() {
+  var driver = new PinoccioPower({
+    device: { connection: 'connect' }
+  });
+
+  it("needs tests");
+});
