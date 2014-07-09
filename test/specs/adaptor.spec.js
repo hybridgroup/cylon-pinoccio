663f61d4beed62a7003691988bdc4e1c14c76fe9
Andrew Stewart <andrew@stwrt.ca>
2014-07-09 12:28:38 -0700

    Improve test coverage

--- test/specs/adaptor.spec.js
+++ test/specs/adaptor.spec.js
@@ -1,9 +1,243 @@
 'use strict';
 
-var Pinoccio = source("adaptor");
+var Cylon = require('cylon'),
+    PinoccioIO = require('pinoccio-io');
+
+var EventEmitter = require('events').EventEmitter;
+
+var Adaptor = source("adaptor");
 
 describe("Cylon.Adaptors.Pinoccio", function() {
-  var adaptor = new Pinoccio();
+  var adaptor;
 
-  it("needs tests");
+  beforeEach(function() {
+    adaptor = new Adaptor({
+      extraParams: {
+        token: 'token',
+        troop: 'troop',
+        scout: 'scout'
+      }
+    })
+  });
+
+  it("subclasses Cylon.Adaptor", function() {
+    expect(adaptor).to.be.an.instanceOf(Cylon.Adaptor);
+    expect(adaptor).to.be.an.instanceOf(Adaptor);
+  });
+
+  describe("constructor", function() {
+    it("sets @token to the provided token", function() {
+      expect(adaptor.token).to.be.eql('token');
+    });
+
+    it("sets @troop to the provided troop", function() {
+      expect(adaptor.troop).to.be.eql('troop');
+    });
+
+    it("sets @scout to the provided scout", function() {
+      expect(adaptor.scout).to.be.eql('scout');
+    });
+  });
+
+  describe("#commands", function() {
+    it("is an array of Pinoccio commands", function() {
+      var commands = adaptor.commands;
+
+      expect(commands).to.be.an('array');
+
+      commands.forEach(function(command) {
+        expect(command).to.be.a('string');
+      });
+    });
+  });
+
+  describe("#connect", function() {
+    var callback;
+
+    beforeEach(function() {
+      callback = spy();
+
+      adaptor.connect(callback);
+    });
+
+    it("sets @board to a Pinoccio instance", function() {
+      expect(adaptor.board).to.be.an.instanceOf(PinoccioIO);
+    });
+
+    context("when the board is ready", function() {
+      it("triggers the callback", function() {
+        adaptor.board.emit('ready');
+        expect(callback).to.be.called;
+      })
+    });
+  });
+
+  describe("read/write functions", function() {
+    beforeEach(function() {
+        adaptor.board = {
+          MODES: {
+            INPUT:  'input',
+            OUTPUT: 'output',
+            ANALOG: 'analog',
+            PWM:    'pwm',
+            SERVO:  'servo'
+          },
+
+          pinMode: spy(),
+
+          digitalRead: spy(),
+          digitalWrite: spy(),
+
+          analogRead: spy(),
+          analogWrite: spy(),
+
+          pwmWrite: spy(),
+          servoWrite: spy()
+        };
+    });
+
+    describe("#digitalRead", function() {
+      var board, callback;
+
+      beforeEach(function() {
+        callback = spy();
+        board = adaptor.board
+
+        adaptor.digitalRead('10', callback);
+      });
+
+      it("sets the board's pin mode to 'input'", function() {
+        expect(board.pinMode).to.be.calledWith('10', 'input');
+      });
+
+      it("tells the board to read from the pin", function() {
+        expect(board.digitalRead).to.be.calledWith('10', callback);
+      });
+    });
+
+    describe("#digitalWrite", function() {
+      var board;
+
+      beforeEach(function() {
+        board = adaptor.board;
+        adaptor.digitalWrite('10', 1);
+      });
+
+      it("sets the board's pin mode to 'output'", function() {
+        expect(board.pinMode).to.be.calledWith('10', 'output');
+      });
+
+      it("tells the board to write to the pin", function() {
+        expect(board.digitalWrite).to.be.calledWith('10', 1);
+      });
+    });
+
+    describe("#analogRead", function() {
+      var board, callback;
+
+      beforeEach(function() {
+        callback = spy();
+        board = adaptor.board
+
+        adaptor.analogRead('10', callback);
+      });
+
+      it("sets the board's pin mode to 'analog'", function() {
+        expect(board.pinMode).to.be.calledWith('10', 'analog');
+      });
+
+      it("tells the board to read from the pin", function() {
+        expect(board.analogRead).to.be.calledWith('10', callback);
+      });
+    });
+
+    describe("#analogWrite", function() {
+      var board;
+
+      beforeEach(function() {
+        board = adaptor.board;
+        adaptor.analogWrite('10', 1);
+      });
+
+      it("sets the board's pin mode to 'analog'", function() {
+        expect(board.pinMode).to.be.calledWith('10', 'analog');
+      });
+
+      it("tells the board to write to the pin", function() {
+        expect(board.analogWrite).to.be.calledWith('10', 255);
+      });
+    });
+
+    describe("#pwmWrite", function() {
+      var board;
+
+      beforeEach(function() {
+        board = adaptor.board;
+        adaptor.pwmWrite('10', 1);
+      });
+
+      it("sets the board's pin mode to 'pwm'", function() {
+        expect(board.pinMode).to.be.calledWith('10', 'pwm');
+      });
+
+      it("tells the board to write to the pin", function() {
+        expect(board.analogWrite).to.be.calledWith('10', 255);
+      });
+    });
+
+    describe("#servoWrite", function() {
+      var board;
+
+      beforeEach(function() {
+        board = adaptor.board;
+        adaptor.servoWrite('10', 1);
+      });
+
+      it("sets the board's pin mode to 'servo'", function() {
+        expect(board.pinMode).to.be.calledWith('10', 'servo');
+      });
+
+      it("tells the board to write to the pin", function() {
+        expect(board.servoWrite).to.be.calledWith('10', 180);
+      });
+    });
+  });
+
+  describe("#pinVal", function() {
+    context("if the value is 1", function() {
+      it("returns 'HIGH'", function() {
+        expect(adaptor.pinVal(1)).to.be.eql('HIGH');
+      });
+    });
+
+    context("if the value is not 1", function() {
+      it("returns 'LOW'", function() {
+        expect(adaptor.pinVal(0)).to.be.eql('LOW');
+      });
+    });
+  });
+
+  describe("#command", function() {
+    var board;
+
+    beforeEach(function() {
+      board = adaptor.board = { command: spy() };
+    });
+
+    it("issues a command to the board with the provided callback", function() {
+      var callback = spy();
+      adaptor.command("hello", callback);
+      expect(board.command).to.be.calledWith("hello", callback);
+    })
+
+    context("if no callback is provided", function() {
+      it("substitutes a no-op function", function() {
+        adaptor.command("hello");
+        var args = board.command.firstCall.args;
+
+        expect(args[0]).to.be.eql('hello');
+        expect(args[1]).to.be.a('function');
+      });
+    });
+  });
 });
--- test/specs/pinoccio-power.spec.js
+++ test/specs/pinoccio-power.spec.js
@@ -1,11 +1,89 @@
 'use strict';
 
-var PinoccioPower = source("pinoccio-power");
+var Cylon = require('cylon');
+
+var Power = source("pinoccio-power");
 
 describe("Cylon.Drivers.PinoccioPower", function() {
-  var driver = new PinoccioPower({
-    device: { connection: 'connect' }
+  var power;
+
+  beforeEach(function() {
+    power = new Power({
+      device: { connection: { command: spy() } }
+    });
   });
 
-  it("needs tests");
+  it("subclasses Cylon.Driver", function() {
+    expect(power).to.be.an.instanceOf(Cylon.Driver);
+    expect(power).to.be.an.instanceOf(Power);
+  });
+
+  describe("#commands", function() {
+    it("is an array of Power commands", function() {
+      var commands = power.commands;
+      expect(commands).to.be.an('array');
+
+      commands.forEach(function(command) {
+        expect(command).to.be.a('string');
+      });
+    });
+  });
+
+  describe("#isCharging", function() {
+    it("tells the connection to print whether or not it's charging", function() {
+      var callback = spy();
+      power.isCharging(callback);
+      expect(power.connection.command).to.be.calledWith(
+        'printf("%b", power.ischarging)',
+        callback
+      );
+    });
+  });
+
+  describe("#percent", function() {
+    it("tells the connection to print the battery percentage", function() {
+      var callback = spy();
+      power.percent(callback);
+      expect(power.connection.command).to.be.calledWith(
+        'printf("%d", power.percent)',
+        callback
+      );
+    });
+  });
+
+  describe("#voltage", function() {
+    it("tells the connection to print the battery voltage", function() {
+      var callback = spy();
+      power.voltage(callback);
+      expect(power.connection.command).to.be.calledWith(
+        'printf("%d", power.voltage)',
+        callback
+      );
+    });
+  });
+
+  describe("#enableVcc", function() {
+    it("tells the connection to enable VCC on the battery", function() {
+      power.enableVcc();
+      expect(power.connection.command).to.be.calledWith("power.enable_vcc");
+    });
+  });
+
+  describe("#disableVcc", function() {
+    it("tells the connection to disable VCC on the battery", function() {
+      power.disableVcc();
+      expect(power.connection.command).to.be.calledWith("power.disable_vcc");
+    });
+  });
+
+  describe("#report", function() {
+    it("tells the connection to report on the battery", function() {
+      var callback = spy();
+      power.report(callback);
+      expect(power.connection.command).to.be.calledWith(
+        'printf("%s", power.report)',
+        callback
+      );
+    });
+  });
 });
